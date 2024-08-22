ffa88358535e3c26a6bec4a7e4a7cec381b6a511
thanknoah <78211701+thanknoah@users.noreply.github.com>
2024-08-22 10:48:54 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,38 +7,38 @@
 #include <vector>
 
 int recursion(int n, int threadNum, bool loggingEnabled) {
-	if (n == 0) {
-		return 0;
-	}
-	if (loggingEnabled) {
-		std::cout << "\nThread [#" << threadNum << "]: " << n;
-	}
-	return n + recursion(n - 1, threadNum, loggingEnabled);
+     if (n == 0) {
+	 return 0;
+     }
+     if (loggingEnabled) {
+        std::cout << "\nThread [#" << threadNum << "]: " << n;
+     }
+     return n + recursion(n - 1, threadNum, loggingEnabled);
 }
 
 std::tuple<int, bool> options() {
-	int numOfFibonnaci;
-	std::string loggingEnabledInput;
+     int numOfFibonnaci;
+     std::string loggingEnabledInput;
 
-	std::cout << "Fibonnaci Simulator V1.0\n";
-	std::cout << "\nEnter number of Fibonacci Numbers >> ";
-	std::cin >> numOfFibonnaci;
-	std::cout << "Enable logging [Yes, No] >> ";
-	std::cin >> loggingEnabledInput;
-	std::transform(loggingEnabledInput.begin(), loggingEnabledInput.end(), loggingEnabledInput.begin(),
+    std::cout << "Fibonnaci Simulator V1.0\n";
+    std::cout << "\nEnter number of Fibonacci Numbers >> ";
+    std::cin >> numOfFibonnaci;
+    std::cout << "Enable logging [Yes, No] >> ";
+    std::cin >> loggingEnabledInput;
+    std::transform(loggingEnabledInput.begin(), loggingEnabledInput.end(), loggingEnabledInput.begin(),
 		[](unsigned char c) { return std::tolower(c); });
 
-	if (loggingEnabledInput == "yes" || loggingEnabledInput == "true") {
-		return std::make_tuple(numOfFibonnaci, true);
-  }
+    if (loggingEnabledInput == "yes" || loggingEnabledInput == "true") {
+        return std::make_tuple(numOfFibonnaci, true);
+    }
 	return std::make_tuple(numOfFibonnaci, false);
-}
+    }
 
-int main() {
+    int main() {
 	  auto listOfOption = options();
 	  auto start = std::chrono::high_resolution_clock::now();
 	  int numOfSequences = std::get<0>(listOfOption);
-      bool loggingEnabled = std::get<1>(listOfOption);
+          bool loggingEnabled = std::get<1>(listOfOption);
 	  int numOfThreads = std::thread::hardware_concurrency();
 
 	  std::vector<std::thread> threads;
@@ -55,4 +55,4 @@ int main() {
 	  std::cout << "\nTime taken: " << elapsed.count() << " seconds" << std::endl;
 
 	  return 0;
-}
+  }
